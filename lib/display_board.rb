e693e825055635b7b4978c3e4fb200ea38ce79c1
sofia tofigh <sst2140@columbia.edu>
2020-02-03 22:09:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,9 @@
-# Define display_board that accepts a board and prints
-# out the current state.
-board = ["X"," "," "," ","O"," "," "," "," "]
-def display_board(the_board)
-  puts"#{the_board[0]} | #{the_board[1]} | #{the_board[2]}"
+def display_board(board=["X"," "," "," ","O"," "," "," "," "])
+  puts"#{board[0]} | #{[1]} | #{[2]}"
   puts"-----------"
-  puts"#{the_board[3]} | #{the_board[4]} | #{the_board[5]}"
+  puts"#{board[3]} | #{[4]} | #{[5]}"
   puts"-----------"
-  puts"#{the_board[6]} | #{the_board[7]} | #{the_board[8]}"
+  puts"#{[6]} | #{[7]} | #{[8]}"
 end
+
+display_board
